e9a6cf70a39a32f78b3228b67f4198769abb774b
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-08-15 12:13:01 +0300

    l

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
 -- Creates a table with fields in the MySQL
-INSERT INTO `first_table` (`id`, `name`) VALUES ('89', 'Holberton School');
+INSERT INTO first_table (id, name)
+VALUES (89, 'Best School');
