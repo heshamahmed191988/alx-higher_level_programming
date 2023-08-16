98fdbbfcdd77db36fb9d8cc7e0dca80afe5423cc
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-08-16 18:12:42 +0300

    l

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,5 @@
--- Creates user
--- create user
+-- creates the MySQL server user user_0d_1
+-- user_0d_1 should have all privileges on your MySQL server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- grant privs
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- flush
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
