866b243a9e8b8d3ce3bf7e74dee03e301214a68e
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-08-16 18:07:25 +0300

    l

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- My privileges
 -- show privs for user1
-SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_1@%;
 -- show privs for user2
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_2@%;
