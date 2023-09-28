5d5bfc2c951a441f9ef01ab2eae07e41d27c126a
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-09-28 06:50:28 -0700

    l

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-url="$1"
-response=$(curl -s -w "%{http_code}" "$url")
-
-if [[ $response == 200 ]]; then
-  body=$(curl -s "$url")
-  echo "$body"
-else
-  echo "Error: Unexpected status code $response"
-fi
+# Curls to the end, location!
+curl -Ls "$Route 2"
