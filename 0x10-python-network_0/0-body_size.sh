dd7150e9beaaed940dba2bc4b3169ea358175eee
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-09-28 07:01:40 -0700

    l

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$10" | grep -i Content-Length | cut -d ' ' -f 2
-
+# Curls request and display bytes received
+curl -so /dev/null -w '%{size_download}\n' "$10"
