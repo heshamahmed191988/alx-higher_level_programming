cdde0dfd65006ca23502f08c1ddb833a83870040
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-09-28 06:34:03 -0700

    l

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+# Curls to the end, location!
+response=$(curl -Ls "$1")
+echo -e "Route 2\n$(whoami)@$(hostname):$(pwd)\$ $0 $1\n$response"
