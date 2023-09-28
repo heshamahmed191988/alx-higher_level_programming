9c8f56743819c05d3e12b82b301928f8b9949fd3
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-09-28 09:29:52 -0700

    l

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends specific header
-curl -s -H "X-HolbertonSchool-User-Id:98" "$1"
+# This script takes a URL as an argument, sends a GET request with a header variable, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash scripts that sends a POST request to a given URL.
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+# This script takes a URL as an argument, sends a POST request with two variables, and displays the body of the response
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
