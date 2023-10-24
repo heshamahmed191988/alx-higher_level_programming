e01108e2a7965c9440161f76fda4c0e5b9b6df5e
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-10-24 12:37:29 +0300

    l

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,24 @@
 #!/usr/bin/node
-// makes get request for SW movie id
+// Import the request module
 const request = require('request');
-request(`http://swapi.co/api/films/${process.argv[2]}`, function (error, response, body) {
-  error && console.log(error);
-  console.log(JSON.parse(body).title);
+
+// Get the movie ID from the first argument
+const movieID = process.argv[2];
+
+// Construct the API endpoint URL
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+// Make a GET request to the API endpoint
+request(url, function (error, response, body) {
+  // Handle any errors
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  // Parse the response body as JSON
+  const data = JSON.parse(body);
+
+  // Print the title of the movie
+  console.log(data.title);
 });
