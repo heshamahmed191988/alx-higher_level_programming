24e9a10489b823012ea5aedb303d4043a2b39a2b
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-10-24 12:41:26 +0300

    l

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,62 @@
 #!/usr/bin/node
-// retrieves all character names in SW film
+// Import the request module
 const request = require('request');
-const FILM_URL = `http://swapi.co/api/films/${process.argv[2]}`;
-let characters;
-const dict = {};
-request(FILM_URL, function (error, response, body) {
+
+// Get the movie ID from the first argument
+const movieID = process.argv[2];
+
+// Construct the API endpoint URL
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+// Make a GET request to the API endpoint
+request(url, function (error, response, body) {
+  // Handle any errors
   if (error) {
-    throw new Error(error);
+    console.error(error);
+    return;
   }
-  characters = JSON.parse(body).characters;
-  for (const url of characters) {
-    request(url, (error, response, body) =>
-      !error && addData(url, JSON.parse(body).name));
+
+  // Parse the response body as JSON
+  const data = JSON.parse(body);
+
+  // Get the array of characters URLs
+  const characters = data.characters;
+
+  // Define an array to store the character names
+  const characterNames = [];
+
+  // Define a counter to keep track of the number of requests completed
+  let counter = 0;
+
+  // Loop through each character URL
+  for (let i = 0; i < characters.length; i++) {
+    // Get the character URL at the current index
+    const characterURL = characters[i];
+
+    // Make another GET request to the character URL
+    request(characterURL, function (error, response, body) {
+      // Handle any errors
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      // Parse the response body as JSON
+      const characterData = JSON.parse(body);
+
+      // Store the character name at the same index in the array
+      characterNames[i] = characterData.name;
+
+      // Increment the counter by one
+      counter++;
+
+      // Check if all requests are completed
+      if (counter === characters.length) {
+        // Print each character name by line in the same order
+        for (let name of characterNames) {
+          console.log(name);
+        }
+      }
+    });
   }
 });
-
-function addData (url, name) {
-  dict[url] = name;
-  if (Object.entries(dict).length === characters.length) {
-    for (const url of characters) {
-      console.log(dict[url]);
-    }
-  }
-}
